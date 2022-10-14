4b39c9e9fbfc728d54e8641623d1ec19579be9c3
Alan <alansan96@hotmail.com>
2022-10-14 21:23:13 +0700

    Initial Commit

---
+++ SwiftUI-Tutorial/SwiftUI_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TutorialApp.swift
+//  SwiftUI-Tutorial
+//
+//  Created by Alan Santoso on 14/10/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
